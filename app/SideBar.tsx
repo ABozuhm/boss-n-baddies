88422cde5826ae6b229dae1234b5cbdca084accd
velora-app <auctionup2305@gmail.com>
2026-04-15 16:35:13 -0400

    Update SideBar.tsx

--- app/SideBar.tsx
+++ app/SideBar.tsx
@@ -1,1 +1,9 @@
-
+export default function Sidebar() {
+  return (
+    <div className="w-40 bg-gray-900 p-2 flex flex-col gap-4">
+      <div className="bg-gray-700 h-20">Ad</div>
+      <div className="bg-gray-700 h-20">Ad</div>
+      <div className="bg-gray-700 h-20">Ad</div>
+    </div>
+  );
+}
