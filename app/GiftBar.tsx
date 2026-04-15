e3eeee72de904c681b1d53f6040dfb800f8e59d1
velora-app <auctionup2305@gmail.com>
2026-04-15 15:50:54 -0400

    Update GiftBar.tsx

--- app/GiftBar.tsx
+++ app/GiftBar.tsx
@@ -1,1 +1,16 @@
 
+export default function GiftBar({ onVote }) {
+  return (
+    <div className="flex justify-around bg-black p-3 rounded-xl">
+      {[1,5,20,100].map(amount => (
+        <button 
+          key={amount}
+          onClick={() => onVote(amount)}
+          className="bg-pink-500 px-4 py-2 rounded-lg text-white"
+        >
+          ${amount}
+        </button>
+      ))}
+    </div>
+  );
+}
