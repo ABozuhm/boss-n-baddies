a7aaee13aef580b0f8f95de4c415fdb9adc86109
velora-app <auctionup2305@gmail.com>
2026-04-15 15:50:19 -0400

    Update SwipeDeck.tsx

--- app/SwipeDeck.tsx
+++ app/SwipeDeck.tsx
@@ -1,1 +1,13 @@
+import TinderCard from "react-tinder-card";
 
+export default function SwipeDeck({ users }) {
+  return (
+    <div className="flex justify-center">
+      {users.map((user) => (
+        <TinderCard key={user.id}>
+          <ContestCard user={user} />
+        </TinderCard>
+      ))}
+    </div>
+  );
+}
