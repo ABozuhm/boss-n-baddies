d5c901ca54321eb5db724d6e7e96bf3d0d584923
velora-app <auctionup2305@gmail.com>
2026-04-15 16:38:29 -0400

    Update voteStore.ts

--- app/voteStore.ts
+++ app/voteStore.ts
@@ -1,21 +1,12 @@
+import { create } from "zustand";
 
-app.post("/vote", async (req, res) => {
-  const { userId, contestantId, amount } = req.body;
-
-  await prisma.vote.create({
-    data: {
-      userId,
-      contestantId,
-      amount
-    }
-  });
-
-  await prisma.contestant.update({
-    where: { id: contestantId },
-    data: {
-      votes_total: { increment: amount }
-    }
-  });
-
-  res.json({ success: true });
-});
+export const useVoteStore = create((set) => ({
+  votes: {},
+  vote: (id, amount) =>
+    set((state) => ({
+      votes: {
+        ...state.votes,
+        [id]: (state.votes[id] || 0) + amount
+      }
+    }))
+}));
